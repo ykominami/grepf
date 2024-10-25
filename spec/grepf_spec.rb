9625286c7d2611359accee0eb7b2b8c640d859f3
ykominami <ykominami@gmail.com>
2024-10-26 04:12:00 +0900

    initial commit

---
+++ spec/grepf_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Grepf do
+  it "has a version number" do
+    expect(Grepf::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
